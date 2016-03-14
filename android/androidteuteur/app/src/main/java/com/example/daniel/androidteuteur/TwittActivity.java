0e1c79e4fb8ee5efc92ac5de076c78d01391cba7
PolSpock <chao-garden@hotmail.com>
2016-03-14 23:12:34 +0100

    TimeLine android sans image

--- android/androidteuteur/app/src/main/java/com/example/daniel/androidteuteur/TwittActivity.java
+++ android/androidteuteur/app/src/main/java/com/example/daniel/androidteuteur/TwittActivity.java
@@ -1,8 +1,12 @@
 package com.example.daniel.androidteuteur;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
@@ -12,7 +16,9 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,44 +29,110 @@ import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by daniel on 11/03/2016.
  */
 public class TwittActivity extends AppCompatActivity {
+    private static Context context;
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    public static Context getAppContext() {
+        return TwittActivity.context;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TwittActivity.context = getApplicationContext();
 
-        new Thread(new Runnable() {
-            public void run() {
+        TwitterTimeLine timeLine = new TwitterTimeLine();
+        Thread t = new Thread(timeLine);
 
-                TwitterAPI twitter = new TwitterAPI();
-                String access_token = "706818055054233600-axPLau20pgEjl752BmJRsVx0Fzaa6ON";
-                String access_token_secret = "TLQ5uK4KK2ZTWnpMA1I8lA6m5IyuoPdCiJWBiB16wNKpz";
+        t.start();
 
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "Je me lance alors que jai pas le droit", Toast.LENGTH_LONG).show();
+
+
+        String value = timeLine.getValue();
+        System.out.println("je suis de retour");
+        System.out.println(value);
+
+                final ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+                 String result = value;
                 try {
+                    System.out.println(result);
 
-                    System.out.println(twitter.getTimeLine(access_token, access_token_secret));
+                    JSONArray jsonArray = new JSONArray(result);
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (HttpException e) {
-                    e.printStackTrace();
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (KeyManagementException e) {
+                    // use
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        String tweetText = jsonObject.getString("text");
+                        String tweetDate = jsonObject.getString("created_at");
+
+                        JSONObject obj = jsonObject.getJSONObject("user");
+
+                        String tweetAuthor = obj.getString("name");
+                        String tweetAlias = obj.getString("screen_name");
+                        String tweetAvatar = obj.getString("profile_image_url");
+
+                        System.out.println(tweetText);
+                        System.out.println(tweetDate);
+                        System.out.println(tweetAuthor);
+                        System.out.println(tweetAlias);
+                        System.out.println(tweetAvatar);
+
+
+                        tweets.add(new Tweet(tweetAuthor, tweetText, tweetDate, tweetAlias, tweetAvatar));
+                    }
+                } catch(Exception e) {
                     e.printStackTrace();
                 }
 
-            }
 
-        }).start();
+
+
+
+
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById( R.id.listView );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter(getAppContext(), R.layout.activity_rowtextview, R.id.rowTextView);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+
+        int listSize = tweets.size();
+
+        for (int i = 0; i < listSize; i++) {
+            listAdapter.add( tweets.get(i).getTweetAuthor() + tweets.get(i).getTweetAlias() + Html.fromHtml("<p style=\"color:red;\">" + tweets.get(i).getTweetText() + "</p>") + Html.fromHtml("<img src=" +tweets.get(i).getTweetAvatar() + ">"));
+        }
+
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
 
     }
 }
