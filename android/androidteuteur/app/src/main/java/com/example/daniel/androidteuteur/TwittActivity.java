6bbde9bbca423de46277d7d2eecd0b031ac8e662
PolSpock <chao-garden@hotmail.com>
2016-03-14 18:26:13 +0100

    Mise en place de l'API sur Android

--- android/androidteuteur/app/src/main/java/com/example/daniel/androidteuteur/TwittActivity.java
+++ android/androidteuteur/app/src/main/java/com/example/daniel/androidteuteur/TwittActivity.java
@@ -2,16 +2,65 @@ package com.example.daniel.androidteuteur;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
 
 /**
  * Created by daniel on 11/03/2016.
  */
-public class TwittActivity extends AppCompatActivity{
+public class TwittActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                TwitterAPI twitter = new TwitterAPI();
+                String access_token = "706818055054233600-axPLau20pgEjl752BmJRsVx0Fzaa6ON";
+                String access_token_secret = "TLQ5uK4KK2ZTWnpMA1I8lA6m5IyuoPdCiJWBiB16wNKpz";
+
+                try {
+
+                    System.out.println(twitter.getTimeLine(access_token, access_token_secret));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (HttpException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (KeyManagementException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }).start();
+
     }
-
-
 }
